9e1c8167a9024d13b7774c5cfb680e85114fd91c
Vincent Arnaud <varnaud@student.42.us.org>
2016-11-12 06:59:39 -0800

    added d format

--- d_conversion.c
+++ d_conversion.c
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 01:06:45 by varnaud           #+#    #+#             */
-/*   Updated: 2016/11/08 00:00:33 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/11/12 06:59:00 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,24 @@ void	d_conversion(t_flags *flags, va_list *args)
 {
 	int		d;
 	char	*n;
+	char	*tmp;
 	int		len;
 
 	d = va_arg(*args, int);
 	len = ft_intlen(d);
-	if (d < 0)
-		len++;
-	if (flags->zero)
+	if (len < flags->width)
 	{
-		if (len < flags->width)
-		{
-
-		}
+		n = (char*)malloc(flags->width + len + 1);
+		if (d < 0)
+			*n = '-';
+		
+		ft_memset(n + (d < 0), flags->zero ? '0' : ' ', flags->width - len);
+		tmp = ft_itoa(d);
+		ft_strcpy(n + (d < 0) + flags->width - len, tmp + (d < 0));
+		free(tmp);
 	}
-	n = ft_itoa(d);
-
-	handle_field(flags, n, ' ');
+	else
+		n = ft_itoa(d);
+	ft_putstr(n);
 	free(n);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/01 14:19:46 by varnaud           #+#    #+#             */
-/*   Updated: 2016/11/04 04:54:49 by varnaud          ###   ########.fr       */
+/*   Updated: 2016/11/07 22:38:30 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ typedef struct	s_flags
 
 int				ft_printf(const char *format, ...)
 				__attribute__ ((format (printf, 1, 2)));
-int				handle_field(t_flags *flags, char *s);
+int				handle_field(t_flags *flags, char *s, char c);
 void			do_conversion(t_flags *flags, va_list *args);
 void			s_conversion(t_flags *flags, va_list *args);
 void			S_conversion(t_flags *flags, va_list *args);
